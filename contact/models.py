29d8e6c661d5ff5b377f05c8500b3926b8841a2a
A.A.Isa <aabdulmajeed.isa@gmail.com>
2020-06-22 17:37:12 +0100

    add about, team, contact and donate apps

---
+++ contact/models.py
@@ -0,0 +1,6 @@
+from django.db import models
+from wagtail.core.models import Page
+from wagtail.admin.edit_handlers import FieldPanel
+from wagtail.images.edit_handlers import ImageChooserPanel
+
+# Create your models here.
