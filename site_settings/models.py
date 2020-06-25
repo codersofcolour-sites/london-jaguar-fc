dd8c23902a9780c09d657f8464bbc4eee1de2f58
A.A.Isa <aabdulmajeed.isa@gmail.com>
2020-06-25 17:46:07 +0100

    improve site settings for logo and social links

--- site_settings/models.py
+++ site_settings/models.py
@@ -3,15 +3,31 @@ from wagtail.admin.edit_handlers import FieldPanel, MultiFieldPanel
 from wagtail.contrib.settings.models import BaseSetting, register_setting
 from wagtail.images.edit_handlers import ImageChooserPanel 
 
-@register_setting
-class SiteSettings(BaseSetting):
+@register_setting(icon='site')
+class SocialMediaSettings(BaseSetting):
   facebook = models.URLField(blank=True, null=True, help_text="Facebook URL")
   twitter = models.URLField(blank=True, null=True, help_text="Twitter URL")
   linkedin = models.URLField(blank=True, null=True, help_text="Linkedin URL")
   instagram = models.URLField(blank=True, null=True, help_text="Instagram URL")
   youtube = models.URLField(blank=True, null=True, help_text="Youtube channel URL")
   pinterest = models.URLField(blank=True, null=True, help_text="Pinterest URL")
+  google = models.URLField(blank=True, null=True, help_text="Google acount URL")
 
+  panels = [
+    FieldPanel('facebook'),
+    FieldPanel('twitter'),
+    FieldPanel('linkedin'),
+    FieldPanel('instagram'),
+    FieldPanel('youtube'),
+    FieldPanel('pinterest'),
+    FieldPanel('google'),
+  ]
+
+  class Meta:
+    verbose_name = 'social media accounts'
+
+@register_setting(icon='image')
+class WebsiteLogo(BaseSetting):
   website_logo = models.ForeignKey(
     "wagtailimages.Image",
     null=True,
@@ -19,15 +35,10 @@ class SiteSettings(BaseSetting):
     on_delete=models.SET_NULL,
     related_name="+",
   )
-  
+
   panels = [
     ImageChooserPanel('website_logo'),
-    MultiFieldPanel([
-      FieldPanel('facebook'),
-      FieldPanel('twitter'),
-      FieldPanel('linkedin'),
-      FieldPanel('instagram'),
-      FieldPanel('youtube'),
-      FieldPanel('pinterest'),
-    ], heading="Social Media Settings"),
   ]
+
+  class Meta:
+    verbose_name = 'Website Logo'
