36645f0f9523bc57478e0399525afac06f95b69f
A.A.Isa <aabdulmajeed.isa@gmail.com>
2020-06-27 14:03:55 +0100

    Modify contact page

--- contact/models.py
+++ contact/models.py
@@ -3,9 +3,20 @@ from wagtail.core.models import Page
 from wagtail.admin.edit_handlers import FieldPanel
 from wagtail.images.edit_handlers import ImageChooserPanel
 
+# class FormField(AbstractFormField):
+#     page = ParentalKey(
+#         'FormPage',
+#         on_delete=models.CASCADE,
+#         related_name='form_fields',
+#     ) 
+
+# class FormPage(AbstractEmailForm):
+
+
 class ContactPage(Page):
     intro = models.CharField(blank=True, max_length=150)
     
     content_panels = Page.content_panels + [
         FieldPanel('intro', classname="full")
     ]
+
