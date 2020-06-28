348b701af96bd203b43d197fb511f1caf26f1e1d
A.A.Isa <aabdulmajeed.isa@gmail.com>
2020-06-28 12:17:30 +0100

    about page done

--- about/models.py
+++ about/models.py
@@ -5,26 +5,17 @@ from wagtail.images.edit_handlers import ImageChooserPanel
 from wagtail.core.fields import StreamField
 from wagtail.core import blocks
 from wagtail.admin.edit_handlers import StreamFieldPanel
-class AboutPage(Page):
-    heading = models.CharField(blank=True, max_length=150)
-    intro_text= models.CharField(blank=True, max_length=350)
-    about_image = models.ForeignKey(
-        'wagtailimages.Image',
-        null=True,
-        blank=False,
-        on_delete=models.SET_NULL
-    )    
+from wagtail.core.rich_text import expand_db_html, RichText
+
+class AboutPage(Page):   
     about_content = StreamField([
+        ('subheading', blocks.RichTextBlock(icon="title", features=['h2'])),
         ('paragraph', blocks.RichTextBlock(icon="pilcrow", features=['p','link', 'bold', 'italic'])),
     ], null=True)
 
+# Replace wrapped rich-text div
+    RichText.__html__ = lambda self: expand_db_html(self.source)
+
     content_panels = Page.content_panels + [
-        MultiFieldPanel(
-            [
-                FieldPanel('heading'),
-                FieldPanel('intro_text')
-            ], heading="Page Header"
-        ),
-        ImageChooserPanel('about_image'),
         StreamFieldPanel('about_content')            
     ]
